7075827fab3545b2d3a2a5c5ad6d7bbe933b923d
Inga <inga.gudaite@gmail.com>
2023-07-27 11:19:22 +0300

    Component features animation

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import useElementOnScreen from '../CustomHooks/useElementOnScreen';
 
 export default function Features() {
+    const [containerRef, isVisible] = useElementOnScreen({
+        root: null,
+        rootMargin: '0px',
+        threshhold: 0.8,
+    });
+
     return (
-        <section className="section-features">
+        <section className="section-features" ref={containerRef}>
             <div className="row">
                 <div className="col-4-of-12">
-                    <div className="feature">
+                    <div
+                        className={`feature ${
+                            isVisible ? 'animated' : ''
+                        }  + feature-1`}
+                    >
                         <i className="fa fa-users icon" />
                         <h3 className="heading-tertiary">Best Experts</h3>
                         <p className="paragraph">
@@ -18,7 +29,11 @@ export default function Features() {
                     </div>
                 </div>
                 <div className="col-4-of-12">
-                    <div className="feature">
+                    <div
+                        className={`feature ${
+                            isVisible ? 'animated' : ''
+                        }  + feature-2`}
+                    >
                         <i className="fa fa-flask icon" />
                         <h3 className="heading-tertiary">Safety Standards</h3>
                         <p className="paragraph">
@@ -31,7 +46,11 @@ export default function Features() {
                     </div>
                 </div>
                 <div className="col-4-of-12">
-                    <div className="feature">
+                    <div
+                        className={`feature ${
+                            isVisible ? 'animated' : ''
+                        }  + feature-3`}
+                    >
                         <i className="fa fa-envelope icon" />
                         <h3 className="heading-tertiary">Gift Vouchers</h3>
                         <p className="paragraph">
